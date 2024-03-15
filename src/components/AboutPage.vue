3d910e4a08101f496756a18e7554297c04fdc34e
Usopked <usopked16496@gmail.com>
2024-03-16 03:29:13 +0900

    오랜만

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,12 +1,28 @@
 <template>
-    <div>
-      <h1>About Page</h1>
+  <div class="flex flex-col pb-9 mx-auto w-full bg-stone-50 max-w-[480px]">
+    <img
+      loading="lazy"
+      srcSet="..."
+      class="w-full aspect-[4.76]"
+    />
+    <div class="flex flex-col px-6 mt-8 w-full">
+      <div class="text-sm text-center text-black">Write Anything</div>
+      <div class="flex flex-col justify-center mt-1.5">
+        <div class="flex flex-col justify-center">
+          <div
+            class="shrink-0 bg-white border border-solid border-slate-300 h-[697px]"
+          ></div>
+        </div>
+      </div>
+      <div
+        class="flex flex-col justify-center self-end mt-2.5 max-w-full text-sm text-center whitespace-nowrap text-zinc-600 w-[107px]"
+      >
+        <div class="flex flex-col justify-center">
+          <div class="justify-center px-9 py-4 rounded-2xl bg-slate-300">
+            POST!
+          </div>
+        </div>
+      </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'AboutPage',
-  }
-  </script>
-  
+  </div>
+</template>
